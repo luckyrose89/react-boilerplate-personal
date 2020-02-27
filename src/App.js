5579db7d6ed5e9d3f48c6cfa0d29ec280ff2e900
avidrutham <divyamathur89@gmail.com>
2020-02-27 17:04:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
